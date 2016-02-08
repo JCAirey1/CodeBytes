cf1c6b86779ffe9be427d64d344ff4c95dbfb0ec
Airey <jairey@SatuitCRM.com>
2016-02-08 14:54:30 -0500

    Readd files

---
+++ MvcApplication1/Models/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    public class TokenController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewToken()
+        {
+            return View();
+        }
+
+    }
+}
